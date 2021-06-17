09eadf3ee899af539bd126baef0a9eac44566748
gnoose <jieli3838@gmail.com>
2021-06-17 11:19:23 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || process.env.PORT);
 }
 bootstrap();
